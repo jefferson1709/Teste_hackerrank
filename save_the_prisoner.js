a1ac89ba2be339a3e43ce98c59691e4804d9eec4
jefferson1709 <jefferson1709@hotmail.com>
2023-01-11 13:08:18 -0300

    inicio do desafio porem ja falhei em outros testes

--- save_the_prisoner.js
+++ save_the_prisoner.js
@@ -2,7 +2,13 @@
 
 function saveThePrisoner(n, m, s) {
     // Write your code here
-    console.log('test' )
+    /* n = total de prisioneiros|m = total de doces|s = assento que se inicia a entrega dos doces 
+        m/n: ciclos de entrega total de doces por tanto queremos o resto -> m%n
+        m%n+s: somamos o resto com o numero da cadeira que iniciou a contagem porem ja tiramos o doce que 
+        entregamos a primeira cadeira -> (m%n+s)-1
+    */
+    res = (m%n+s)-1
+    console.log(res )
 
 }
-saveThePrisoner();
+saveThePrisoner(7,19,3); // 7

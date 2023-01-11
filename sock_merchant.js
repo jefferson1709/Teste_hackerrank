1ff3cfab55bbe429b48710876008136ff2112d97
jefferson1709 <jefferson1709@hotmail.com>
2023-01-11 17:36:24 -0300

    Sock Merchant: depois de 6 teste

--- sock_merchant.js
+++ sock_merchant.js
@@ -3,24 +3,29 @@
 function sockMerchant(n, ar) {
     // Write your code here
     /*  n = total de de meias na pilha|ar[i] = cores de cada meia
+        alinhamos os componentes    
         realizamos uma varedura nos componentes
+        comparamos um componente com outro na sequencia
        
     */
     /* Aplicando as restriçoes 
-        se  n=<1 e n=>100 -> retona para erro
-        se ar[1]< == 0 -> retona para n 
+        se  n=<1 e n=>100 -> retona 0
+        se ar[1]< == 0 -> retona 0 
     */
-    for(index in ar) {
-        console.log(ar[index]);
-    }
+    if (!ar || ar.length !== n  ) return 0;
+    let par = 0;
+    ar=ar.sort();
+    for(i = 0; i < n; i++) {
+        if(ar[i] == ar[i+1]){
+            par++;
+            i++;
+        };
 
-
-        
-
-
-
+    };
+    console.log(par);
+    return par;
 }
 
 
 sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]); //3
-//sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 10, 20]); //3
+sockMerchant(9, [10, 20, 20, 10, 10, 30, 50, 20, 20]); //3

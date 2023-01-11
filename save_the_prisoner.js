b79a5b1a2c2022f33d87eea0e3bda467d3d54557
jefferson1709 <jefferson1709@hotmail.com>
2023-01-11 12:51:23 -0300

    preparando o ambiente de teste

---
+++ save_the_prisoner.js
@@ -0,0 +1,8 @@
+// link do Exercicio: https://www.hackerrank.com/challenges/save-the-prisoner/problem
+
+function saveThePrisoner(n, m, s) {
+    // Write your code here
+    console.log('test' )
+
+}
+saveThePrisoner();
